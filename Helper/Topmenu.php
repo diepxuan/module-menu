7246b53a3fe9d45a2718be9edca7262ed21855d8
Tran Ngoc Duc <caothu91@gmail.com>
2017-10-04 17:55:18 +0700

    home | detect is active

--- Helper/Topmenu.php
+++ Helper/Topmenu.php
@@ -55,7 +55,7 @@ class Topmenu
             'name'       => __('Home'),
             'id'         => 'home',
             'url'        => '/',
-            'has_active' => true,
+            'has_active' => $this->isHomepage(),
             'is_active'  => $this->isHomepage(),
         ];
     }
